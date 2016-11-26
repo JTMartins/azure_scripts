358e1f68e7c4687073e41eaa9c8e6999625c91e7
José Martins <martins1966@gmail.com>
2016-11-26 13:30:27 +0000

    first deploy
    notworking yet

---
+++ install_backend.sh
@@ -0,0 +1,94 @@
+﻿#!/bin/bash
+
+apt-get -y update
+
+apt-get -y install python-software-properties
+
+add-apt-repository -y ppa:ondrej/php5-5.6
+
+apt-get -y update
+
+
+
+# set up a silent install of MySQL
+
+dbpass=$1
+
+SharedStorageAccountName=$2
+SharedAzureFileName=$3
+SharedStorageAccountKey=$4
+
+
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+
+
+# install the LAMP stack
+
+apt-get -y install apache2 mysql-client mysql-server php5.6
+
+
+
+# install Extra PHP requirements
+
+apt-get -y install php5.6-curl php5.6-gd php5-ldap php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+
+
+
+# Allow remote connection
+
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+
+
+# create moodle database
+
+MYSQL=`which mysql`
+
+
+
+Q1="CREATE DATABASE moodle DEFAULT CHARACTER SET UTF8 COLLATE utf8_unicode_ci;"
+
+Q2="GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'root'@'%' IDENTIFIED BY '$dbpass' WITH GRANT OPTION;"
+
+Q3="FLUSH PRIVILEGES;"
+
+SQL="${Q1}${Q2}${Q3}"
+
+
+
+$MYSQL -uroot -p$dbpass -e "$SQL"
+
+# Create Azure file share that will be used by front end VM's for moodledata directory
+
+
+
+apt-get -y install nodejs-legacy
+
+apt-get -y install npm
+
+npm install -g azure-cli
+
+
+
+sudo azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
+
+
+
+
+
+
+# restart MySQL
+
+service mysql restart
+
+
+
+# restart Apache
+
+apachectl restart
---
+++ install_frontend.sh
@@ -0,0 +1,49 @@
+﻿#!/bin/bash
+apt-get -y update
+apt-get -y install python-software-properties
+add-apt-repository -y ppa:ondrej/php5-5.6
+apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=$1
+SharedStorageAccountName=$2
+SharedAzureFileName=$3
+FullNameOfSite=$4
+ShortNameOfSite=$5
+MoodleAdminUser=$6
+MoodleAdminPass=$7
+MoodleAdminEmail=$8
+moodleVersion=$9
+installOfficePlugins=$10
+SharedStorageAccountKey=$11
+LoadbalancerFqdn=$12
+DbFqdn=$13
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# install the LAMP stack
+apt-get -y install apache2 mysql-client mysql-server php5.6
+
+# install Extra PHP requirements
+
+apt-get -y install php5.6-curl php5.6-gd php5-ldap php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+
+# add port 8000 for admin access
+perl -0777 -p -i -e 's/Listen 80/Listen 80\nListen 8080/ig' /etc/apache2/ports.conf
+perl -0777 -p -i -e 's/\*:80/*:80 *:8080/g' /etc/apache2/sites-enabled/000-default.conf
+
+apt-get install unzip
+
+
+
+
+# restart Apache
+apachectl restart
+
+
+
+#resolve domain name to ip address
+wwwrootval="http://$(resolveip -s $LoadbalancerFqdn):80/"
+DbIpAddress=$(resolveip -s $DbFqdn)
