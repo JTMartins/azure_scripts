f857ffa9d61cc00b017d484c6e839232fbcb6309
José Martins <martins1966@gmail.com>
2016-11-26 14:34:44 +0000

    Correcting Scripts for PHP5-oldStable

--- install_backend.sh
+++ install_backend.sh
@@ -1,92 +1,60 @@
 ﻿#!/bin/bash
-
-sudo apt-get -y update
-
-sudo apt-get -y install python-software-properties
-
-sudo add-apt-repository -y ppa:ondrej/php
-
-sudo apt-get -y update
-
-
+apt-get -y update
+apt-get -y install python-software-properties
+add-apt-repository -y ppa:ondrej/php5-oldstable
+apt-get -y update
 
 # set up a silent install of MySQL
-
 dbpass=$1
-
 SharedStorageAccountName=$2
 SharedAzureFileName=$3
 SharedStorageAccountKey=$4
 
-
-
 export DEBIAN_FRONTEND=noninteractive
-
-sudo echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-
-sudo echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
-
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
+apt-get -y install apache2 mysql-client mysql-server php5
 
-sudo apt-get -y install apache2 mysql-client mysql-server php5.6
-
-
-
-# install Extra PHP requirements
-
-sudo apt-get -y install php5.6-curl php5.6-gd php5-ldap php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
-
-
+# install moodle requirements
+apt-get -y install graphviz aspell php5-pspell php5-curl php5-gd php5-intl php5-mysql php5-xmlrpc php5-ldap php5-mbstring php5-mcrypt php5-mysql php5-xml
 
 # Allow remote connection
-
-sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-
-
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 # create moodle database
-
 MYSQL=`which mysql`
 
-
-
 Q1="CREATE DATABASE moodle DEFAULT CHARACTER SET UTF8 COLLATE utf8_unicode_ci;"
-
 Q2="GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'root'@'%' IDENTIFIED BY '$dbpass' WITH GRANT OPTION;"
-
 Q3="FLUSH PRIVILEGES;"
-
 SQL="${Q1}${Q2}${Q3}"
-
-
 
 $MYSQL -uroot -p$dbpass -e "$SQL"
 
 # Create Azure file share that will be used by front end VM's for moodledata directory
 
+apt-get -y install nodejs-legacy
+apt-get -y install npm
+npm install -g azure-cli
 
+sudo azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
 
-sudo apt-get -y install nodejs-legacy
+# restart MySQL
+service mysql restart
 
-sudo apt-get -y install npm
+# restart Apache
+apachectl restart
 
 sudo npm install -g azure-cli
 
-
-
 azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
-
-
-
-
 
 
 # restart MySQL
 
 sudo service mysql restart
-
 
 
 # restart Apache
--- install_frontend.sh
+++ install_frontend.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/bash
 sudo apt-get -y update
 sudo apt-get -y install python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php
+add-apt-repository -y ppa:ondrej/php5-oldstable
 sudo apt-get -y update
 
 # set up a silent install of MySQL
@@ -20,11 +20,11 @@ sudo echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf
 sudo echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-sudo apt-get -y install apache2 mysql-client mysql-server php5.6
+sudo apt-get -y install apache2 mysql-client mysql-server php5
 
 # install Extra PHP requirements
 
-sudo apt-get -y install php5.6-curl php5.6-gd php5-ldap php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+sudo apt-get -y install php5-curl php5-gd php5-ldap php5-mbstring php5-mcrypt php5-mysql php5-xml
 
 # add port 8000 for admin access
 sudo perl -0777 -p -i -e 's/Listen 80/Listen 80\nListen 8080/ig' /etc/apache2/ports.conf
